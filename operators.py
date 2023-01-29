c63a8bea7f447af55e2818a42f9f4f3f946d18b4
Anant Goel <90477164+Anant2002@users.noreply.github.com>
2023-01-29 14:53:07 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,19 @@
+a= 4
+b= 3
+
+print("The addition of a and b is", a+b)
+print("The subtraction of b from a is", a-b)
+print("The multiplication of a and b is", a*b)
+print("The division of a and b is", a/b)
+print("The modulus of a and b is", a%b)
+print("The floor division of a and b is", a//b)
+print("The exponent of a and b is", a**b)
+
+# OUTPUTS:- 
+# The addition of a and b is 7
+# The subtraction of b from a is 1
+# The multiplication of a and b is 12
+# The division of a and b is 1.3333333333333333
+# The modulus of a and b is 1
+# The floor division of a and b is 1
+# The exponent of a and b is 64
---
+++ userinput.py
@@ -0,0 +1,16 @@
+a= input()
+print(a)
+
+b= input("Enter message here: ")
+print(b)
+
+c= int(input())
+print(c)
+
+# OUTPUT:
+# 2
+# 2
+# Enter message here: 12
+# 12
+# 23
+# 23
